658be705379d8a51d7da0320f67a91d4102cffa3
Echo Chen <echo.chen@umoregroup.com>
2018-11-29 11:18:03 +0800

    0.1.3

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 'use strict';
 const path = require('path');
 const { pit, superpit } = require('radishpit');
-const { initializer } = require('./lib');
+const pitFactory = require('./lib/pit-factory');
 
 module.exports = app => {
   app.loader.loadToApp(path.join(app.baseDir, 'app/schema'), 'schema', {
     ignore: 'types.js',
     call: false,
-    initializer,
+    initializer: (schema) => {
+      if (typeof schema === 'function') {
+        return pitFactory(schema(app));
+      } else {
+        return pitFactory(schema);
+      }
+    },
   });
   app.radishpit = {
     pit,
